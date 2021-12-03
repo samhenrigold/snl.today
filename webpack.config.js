2578b3c05fe4be3834d74bb3f8da21ba9f2c2304
samhenrigold <samhenrigold@gmail.com>
2021-12-02 19:42:07 -0500

    *bonk* no more webpack!

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/assets/dateset.js',
-  output: {
-    path: path.resolve(__dirname, '_site/assets'),
-    filename: 'dateset.js'
-  }
-};
